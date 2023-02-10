acf19c8befcd7771f722826a0a913b17a56a09d0
davidcoats <david@getsafetycone.com>
2023-02-09 17:29:30 -0800

    Initial commit.

---
+++ source/R5T.Z0029/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.Z0029
+{
+	/// <summary>
+	/// C# language keywords.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.Z0029/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.Z0029
+C# language keywords.
